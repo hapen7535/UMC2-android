0fe8051921575d972afaf60a2bde5d5355475925
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-03-26 19:23:27 +0900

    2주차 실습 : Fragment 전환

--- app/src/main/java/com/example/example1/MainActivity.kt
+++ app/src/main/java/com/example/example1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.example1.databinding.ActivityMainBinding
+import com.example.example1.databinding.FragmentAlbumBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +15,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initBottomNavigation()
+
         binding.homePannelPlayIv.setOnClickListener{
 
             startActivity(Intent(this, SongActivity::class.java))
 
         }
-
+        binding.homePannelAlbumIv.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, AlbumFragment())
+                .commitNowAllowingStateLoss()
+        }
 
     }
+
+    private fun initBottomNavigation(){
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitNowAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+
 }
