ee1f13c4eca5a0e9677eb3d045e6517718d3f3b3
Yuhee Kim <dbgml4509@naver.com>
2022-04-10 14:21:23 +0900

    4주차 스플래쉬 화면 구현

--- app/src/main/java/com/example/example1/MainActivity.kt
+++ app/src/main/java/com/example/example1/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Example1)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
