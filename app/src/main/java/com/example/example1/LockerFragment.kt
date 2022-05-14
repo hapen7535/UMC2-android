745cc5e20b0c9fcc70f1ba059c646f02e93b0643
Yuhee Kim <dbgml4509@naver.com>
2022-05-14 16:14:27 +0900

    8주차 실습 : 로그인 기능 구현

--- app/src/main/java/com/example/example1/LockerFragment.kt
+++ app/src/main/java/com/example/example1/LockerFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.example1.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayout
@@ -36,5 +37,38 @@ class LockerFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onStart() {
+        super.onStart()
+        initView()
+    }
+
+    private fun getJwt():Int{
+        val spf =activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE) //Fragment에서는 AppCompatActivity.?로 적음
+        return spf!!.getInt("jwt", 0) //없으면 0을 반환
+    }
+
+    private fun initView(){
+        val jwt : Int = getJwt()
+        if(jwt == 0){
+            binding.lockerLoginTv.text = "로그인"
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+        }else{
+            binding.lockerLoginTv.text = "로그아웃"
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun logout(){
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        editor.remove("jwt")
+        editor.apply()
+    }
+
 
 }
--- app/src/main/java/com/example/example1/LoginActivity.kt
+++ app/src/main/java/com/example/example1/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.example1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.example1.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -16,6 +18,52 @@ class LoginActivity : AppCompatActivity() {
         binding.loginSignUpTv.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+        binding.loginSignInBtn.setOnClickListener{
+            login()
+        }
 
+
+    }
+
+    private fun login(){
+        //입력하지 않은 곳이 없는지 확인을 하는 validation 체크가 필요
+        if (binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){ //이메일이 비어있을 경우
+            Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (binding.loginPasswordEt.text.toString().isEmpty() ){ //비밀번호가 비어있을 경우
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val email : String = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        val pwd : String = binding.loginPasswordEt.text.toString()
+
+        //DB에 사용자가 입력한 정보가 존재하는지 확인
+        val songDB = SongDatabase.getInstance(this)!! //songDB 연결
+        val user = songDB.UserDao().getUser(email,pwd)
+
+        //DB에 사용자의 정보가 있는 경우
+        user?.let{
+            Log.d("LOGIN_ACT/GET_USER", "userId : ${user.id}, $user")
+            saveJwt(user.id)
+        }
+
+        //사용자가 입력한 정보가 DB에 없다면
+        Toast.makeText(this, "회원정보가 존재하지 않습니다", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun saveJwt(jwt:Int){
+        val spf = getSharedPreferences("auth", MODE_PRIVATE)
+        val editor = spf.edit()
+
+        editor.putInt("jwt",jwt) //키 값은 받은 jwt 값이 된다.
+        editor.apply()
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
